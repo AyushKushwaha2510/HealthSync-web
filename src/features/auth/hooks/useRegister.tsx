b678fb94182224555c679c03414e9df665ba1b50
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-03 22:58:00 +0530

    user registration done

---
+++ src/features/auth/hooks/useRegister.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { RegisterUserDto } from "../types/register.dto";
+import { registerApi } from "../api/auth.api";
+
+export const useRegister = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [success, setSuccess] = useState<string | null>(null);
+
+    const register = async (data: RegisterUserDto) => {
+        try {
+            setLoading(true);
+            setError(null);
+
+            const res = await registerApi(data);
+            console.log("rers", res)
+            setSuccess(res.message)
+            return res;
+
+        } catch (err: any) {
+            console.log("error", err.response)
+            setError(err.response?.data.message || "Registration failed");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { register, loading, success, error };
+};
