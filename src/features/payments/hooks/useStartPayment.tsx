bb7621d5d98ef5a3c76279851e3a43ea451dcb8b
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-21 17:53:34 +0530

    Add useStartPayment hook for handling payment processing with Razorpay

---
+++ src/features/payments/hooks/useStartPayment.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useState } from "react";
+import { createOrderApi, verifySignatureApi } from "../api/payment.api";
+import { CreateOrder } from "../types/payment.type";
+
+declare global {
+  interface Window {
+    Razorpay: any;
+  }
+}
+
+export const useStartPayment = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
+
+  const startPayment = async (data: CreateOrder) => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      // Create Order
+      const orderRes = await createOrderApi(data);
+      const orderData = orderRes.data;
+
+      // Open Razorpay
+      const options = {
+        key: process.env.RAZORPAY_KEY_ID,
+        amount: orderData.amount,
+        currency: orderData.currency,
+        order_id: orderData.id,
+
+        handler: async (response: any) => {
+          // Verify payment
+          const res = {
+            externalOrderId: response.razorpay_order_id,
+            externalPaymentId: response.razorpay_payment_id,
+            externalSignature: response.razorpay_signature,
+          }
+
+          await verifySignatureApi(res)
+
+        },
+      };
+
+      const razor = new window.Razorpay(options);
+      razor.open();
+    }
+    catch (error: any) {
+      setSuccess(null);
+      setError(
+        error.response?.data?.message ||
+        'Failed to Process Payment Request'
+      );
+    }
+  }
+
+  return { startPayment, loading, success, error };
+}
