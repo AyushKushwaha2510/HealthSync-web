4cd071930fcf239399cf1aabf5432c06e0f83793
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-22 16:42:30 +0530

    Remove commented import for PatientDetailsDto in Doctor type definition

--- src/features/doctors/types/doctor.type.ts
+++ src/features/doctors/types/doctor.type.ts
@@ -1,7 +1,7 @@
 import { User } from '@/features/auth/types/user.type';
 import { Clinic } from '@/features/clinics/types/clinic.type';
 import { Hospital } from '@/features/hospitals/types/hospital.type';
-import { PatinetDetailsDto } from '@/features/patients/types/patient';
+// import { PatinetDetailsDto } from '@/features/patients/types/patient';
 
 export type Doctor = {
   id: string;
