957d40e018a73131919fdb782fab9d62c589fab9
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-23 18:28:36 +0530

    Enhance layout in Page component by adding flex class to Container and including AppointmentSummary

--- src/app/appointments/book/[doctor_id]/page.tsx
+++ src/app/appointments/book/[doctor_id]/page.tsx
@@ -1,11 +1,13 @@
 import Container from "@/components/Container";
+import AppointmentSummary from "@/features/appointments/components/AppointmentSummary";
 import DoctorAvailabilityDetails from "@/features/doctors-availability/components/DoctorAvailabilityDetails";
 
 export default async function Page({ params }: { params: Promise<{ doctor_id: string }> }) {
   const { doctor_id } = await params;
   return (
-    <Container>
+    <Container className="flex">
       <DoctorAvailabilityDetails id={doctor_id} />
+      <AppointmentSummary/>
     </Container>
   )
 }
