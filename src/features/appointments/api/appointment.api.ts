86a2042380f25984f11aac4405039e1566641ec9
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-25 11:55:53 +0530

    Add API functions for retrieving and booking appointments

--- src/features/appointments/api/appointment.api.ts
+++ src/features/appointments/api/appointment.api.ts
@@ -1,6 +1,12 @@
 import api from '@/lib/axios';
+import { BookAppointment } from '../types/appointment.type';
 
 export const getAllAppointmentsApi = async () => {
   const res = await api.get('/appointments');
+  return res.data;
+};
+
+export const bookAppointmentsApi = async (data: BookAppointment) => {
+  const res = await api.post('/appointments', data);
   return res.data;
 };
