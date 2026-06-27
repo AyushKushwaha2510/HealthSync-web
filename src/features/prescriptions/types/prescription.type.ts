588dc8241ca5c89193a9ebdf46a860690fbf3f3b
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-27 18:02:35 +0530

    Add prescription types for managing prescriptions and associated medicines

---
+++ src/features/prescriptions/types/prescription.type.ts
@@ -0,0 +1,20 @@
+import { Appointment } from '@/features/appointments/types/appointment.type';
+
+export type AddPrescription = {
+  appointmentId: string;
+  medicines?: Medicine[];
+  notes?: string[];
+};
+
+export interface Prescription extends AddPrescription {
+  id: string;
+  appointment: Partial<Appointment>;
+}
+
+export type Medicine = {
+  name: string;
+  dosage: string;
+  frequency: string;
+  duration: string;
+  note?: string;
+};
