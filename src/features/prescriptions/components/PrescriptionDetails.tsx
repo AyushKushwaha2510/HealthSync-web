7ba644fac0b97d9511a41c09ecb77d112228a82a
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-06-30 01:15:19 +0530

    Refactor PrescriptionDetails to fetch and pass prescription data to PrescriptionForm

--- src/features/prescriptions/components/PrescriptionDetails.tsx
+++ src/features/prescriptions/components/PrescriptionDetails.tsx
@@ -1,14 +1,23 @@
+import { getAppointmentInfo } from "@/features/appointments/services/getAppointmentInfo";
 import PrescriptionActions from "./ActionButtons";
 import PrescriptionForm from "./PrescriptionForm";
 
-export default function PrescriptionDetails({ appointmentId }: { appointmentId: string }) {
+export default async function PrescriptionDetails({
+  appointmentId,
+}: {
+  appointmentId: string
+}) {
+  const appointment = await getAppointmentInfo(appointmentId);
+  const prescription = appointment.prescription;
+
   return (
     <div className="space-y-5">
       <PrescriptionActions
         appointmentId={appointmentId}
       />
-      
+
       <PrescriptionForm
+        prescription={prescription}
         appointmentId={appointmentId}
       />
     </div>
