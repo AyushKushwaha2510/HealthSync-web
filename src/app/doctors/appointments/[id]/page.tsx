5df15c99576b428539dae82baae3de7639625a3c
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-05 20:24:39 +0530

    Add Page component to display appointment and prescription details

---
+++ src/app/doctors/appointments/[id]/page.tsx
@@ -0,0 +1,19 @@
+import Container from "@/components/Container";
+import AppointmentDetails from "@/features/appointments/components/AppointmentDetails";
+import PrescriptionDetails from "@/features/prescriptions/components/PrescriptionDetails";
+
+export default async function Page({
+  params
+}: {
+  params: Promise<{ id: string }>
+}) {
+
+  const { id } = await params;
+
+  return (
+    <Container className="!mt-0 gap-5 items-start">
+      <AppointmentDetails id={id} />
+      <PrescriptionDetails appointmentId={id} />
+    </Container>
+  )
+}
