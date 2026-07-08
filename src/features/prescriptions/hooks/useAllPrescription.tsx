5d129caae0d1a31f46a3492564846a815f08975d
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-08 14:32:22 +0530

    Add custom hook useAllPrescription to fetch and manage prescription data

---
+++ src/features/prescriptions/hooks/useAllPrescription.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { getAllPrescriptionsApi } from "../api/prescription.api";
+import { Prescription } from "../types/prescription.type";
+
+export const useAllPrescription = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [prescriptions, setPrescriptions] = useState<Partial<Prescription[] | null>>(null)
+
+  const fetchAllPrescription = async (doctorId: string, patientId: string) => {
+
+    try {
+      setLoading(true);
+
+      const res = await getAllPrescriptionsApi(doctorId, patientId);
+
+      setPrescriptions(res)
+
+    }
+    catch (error: any) {
+      console.error("error", error.response.data.message)
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  return { fetchAllPrescription, prescriptions, loading }
+}
