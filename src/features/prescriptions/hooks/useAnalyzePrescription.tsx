44dd91a29a4f2130c9556f0769cc959762452f9e
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-09 00:08:04 +0530

    Add useAnalyzePrescription hook for prescription analysis functionality

---
+++ src/features/prescriptions/hooks/useAnalyzePrescription.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { analyzePrescriptionApi } from "../api/prescription.api";
+import { PrescriptionAnalysis } from "../types/prescription.type";
+
+export const useAnalyzePrescription = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [analysis, setAnalysis] = useState<PrescriptionAnalysis>()
+
+  const analyzePrescription = async (prescriptionId: string) => {
+
+    try {
+      setLoading(true);
+
+      const res = await analyzePrescriptionApi(prescriptionId);
+
+      setAnalysis(res)
+
+    }
+    catch (error: any) {
+      console.error("error", error.response.data.message)
+    }
+    finally {
+      setLoading(false);
+    }
+  }
+
+  return { analyzePrescription, analysis, loading }
+}
