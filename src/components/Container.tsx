88e52d9846c837b00ddf78ba5336cc1545c570bb
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-08 14:36:49 +0530

    Update Container component to apply flexbox centering when centered prop is true

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -11,7 +11,7 @@ export default function Container({
 }: ContainerProps) {
   return (
     <div
-      className={`${centered ? 'mx-auto' : ''} w-full max-w-[100rem] mt-25 p-2 ${className}`}
+      className={`${centered ? 'flex items-center justify-center' : ''} w-full max-w-[100rem] mt-25 p-2 ${className}`}
     >
       {children}
     </div>
