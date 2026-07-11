6e19ad626ca2f58623335d8ca7557108f02ee5ff
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-11 12:40:56 +0530

    Refactor PrescriptionCard props to use 'analysis' instead of 'prescription' and clean up unused code

--- src/features/chatbot/components/cards/PrescriptionCard.tsx
+++ src/features/chatbot/components/cards/PrescriptionCard.tsx
@@ -4,7 +4,6 @@ import {
   ClipboardList,
   HeartPulse,
   Pill,
-  TriangleAlert,
 } from "lucide-react";
 import {
   Card,
@@ -20,67 +19,11 @@ interface PrescriptionCardProps {
 export function PrescriptionCard({
   analysis,
 }: PrescriptionCardProps) {
-  // return (
-  //   <div className="space-y-5">
-  //     {/* Header */}
-  //     <div className="flex items-center gap-3">
-  //       <div className="rounded-xl bg-blue-100 p-2">
-  //         <Pill className="h-5 w-5 text-blue-600" />
-  //       </div>
 
-  //       <div>
-  //         <h3 className="font-semibold">
-  //           Prescription Analysis
-  //         </h3>
+  if (!analysis) return <p>Unable to Analyise</p>
 
-  //         <p className="text-sm text-muted-foreground">
-  //           Medicines detected
-  //         </p>
-  //       </div>
-  //     </div>
-
-  //     {/* Medicines */}
-  //     <div className="space-y-3">
-  //       {prescription.medicines.map((medicine) => (
-  //         <div
-  //           key={medicine.name}
-  //           className="rounded-xl border p-3"
-  //         >
-  //           <div className="font-medium">
-  //             {medicine.name}
-  //           </div>
-
-  //           <div className="mt-1 text-sm text-muted-foreground">
-  //             {medicine.dosage}
-  //           </div>
-
-  //           {/* <div className="text-sm text-muted-foreground">
-  //             {medicine.frequency}
-  //           </div> */}
-  //         </div>
-  //       ))}
-  //     </div>
-
-  //     {/* Warnings */}
-  //     {prescription.warnings.length > 0 && (
-  //       <div className="rounded-xl border border-amber-200 bg-amber-50 p-4">
-  //         <div className="mb-3 flex items-center gap-2 font-medium text-amber-700">
-  //           <TriangleAlert className="h-5 w-5" />
-  //           Warnings
-  //         </div>
-
-  //         <ul className="space-y-2 text-sm text-amber-700">
-  //           {prescription.warnings.map((warning) => (
-  //             <li key={warning}>• {warning}</li>
-  //           ))}
-  //         </ul>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
-  console.log("analysis ", analysis)
   return (
-    <div className="space-y-6 max-h-[75vh] overflow-y-auto pr-2">
+    <div className="space-y-6 max-h-[75vh] overflow-y-auto pr-2 min-w-2xl">
 
       {/* Summary */}
       <Card>
