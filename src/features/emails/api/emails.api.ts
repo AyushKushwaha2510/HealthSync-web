338d91e8d2dd4947d579093052ab79b10967cd0b
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2026-07-26 11:35:30 +0530

    Refactor email API functions to improve structure and consistency

--- src/features/emails/api/emails.api.ts
+++ src/features/emails/api/emails.api.ts
@@ -1,10 +1,16 @@
 import api from '@/lib/axios';
-import { EmailVerificationMail } from '../types/emails.type';
+import { EmailVerificationMail, VerifyMailOtp } from '../types/emails.type';
 
-export const sendEmailVerificationMailApi = async (data: EmailVerificationMail) => {
-  const res = await api.post(
-    'emails/send-email-verification-mail', 
-    data
-  );
+export const sendEmailVerificationMailApi = async (
+  data: EmailVerificationMail,
+) => {
+  const res = await api.post('/emails/send-email-verification-mail', data);
+  return res.data;
+};
+
+export const verifyMailOtpApi = async (
+  data: VerifyMailOtp
+) => {
+  const res = await api.post('/emails/verify-mail-otp', data);
   return res.data;
 };
